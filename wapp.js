2a61544add8543dbcb1c4febb69858898b3dda21
Rene Pupala <45124373+MsTiik@users.noreply.github.com>
2021-11-09 23:55:20 +0800

    asdawsd

--- wapp.js
+++ wapp.js
@@ -43,7 +43,9 @@ let sortSpan = document.createElement("span");
 let regionSpan = document.createElement("span");
 
 let sort = document.createElement("select");
+sort.id = "sort";
 let region = sort.cloneNode();
+region.id = "region";
 let opiton = document.createElement("OPTION");
 
 let sortO1 = opiton.cloneNode(true);
@@ -58,11 +60,11 @@ sortDefault.innerHTML = "Sort By";
 
 sort.appendChild(sortDefault);
 sort.appendChild(sortO1);
-sortO1.innerHTML = "<p>Area</a>";
+sortO1.innerHTML = "Area";
 sort.appendChild(sortO2);
-sortO2.innerHTML = "<p>Name</a>";
+sortO2.innerHTML = "Name";
 sort.appendChild(sortO3);
-sortO3.innerHTML = "<p>Population</a>";
+sortO3.innerHTML = "Population";
 
 let regionO1 = opiton.cloneNode(true);
 let regionO2 = opiton.cloneNode(true);
@@ -80,23 +82,23 @@ regionDefault.innerHTML = "By Region";
 
 region.appendChild(regionDefault);
 region.appendChild(regionO1);
-regionO1.innerHTML = "<p>All</a>";
+regionO1.innerHTML = "All";
 region.appendChild(regionO2);
-regionO2.innerHTML = "<p>Africa</a>";
+regionO2.innerHTML = "Africa";
 region.appendChild(regionO3);
-regionO3.innerHTML = "<p>Americas</a>";
+regionO3.innerHTML = "Americas";
 region.appendChild(regionO4);
-regionO4.innerHTML = "<p>Asia</a>";
+regionO4.innerHTML = "Asia";
 region.appendChild(regionO5);
-regionO5.innerHTML = "<p>Europe</a>";
+regionO5.innerHTML = "Europe";
 region.appendChild(regionO6);
-regionO6.innerHTML = "<p>Oceania</a>";
+regionO6.innerHTML = "Oceania";
 region.appendChild(regionO7);
-regionO7.innerHTML = "<p>Polar</a>";
+regionO7.innerHTML = "Polar";
 
 searchSortBar.appendChild(sortRegionSpan);
-sortRegionSpan.appendChild(sort);
 sortRegionSpan.appendChild(region);
+sortRegionSpan.appendChild(sort);
 
 // displays order by and shown region info
 let displayStatus = document.createElement("div");
@@ -105,18 +107,26 @@ displayStatus.setAttribute("id", "display-status");
 searchSortBar.appendChild(displayStatus);
 
 // p and span of display information{}
-let dsOrderBy = document.createElement("p");
-let orderByT = document.createTextNode("Order By: ${order}");
-let dsShowedRegion = document.createElement("span");
-let showedRegionT = document.createTextNode("Showed Region: ${region} ");
-dsOrderBy.appendChild(orderByT);
-dsOrderBy.id = "order-by";
-dsShowedRegion.appendChild(showedRegionT);
-dsShowedRegion.id = "showed-region";
-displayStatus.appendChild(dsOrderBy);
-dsOrderBy.appendChild(dsShowedRegion);
+let underSearch = document.createElement("div");
+underSearch.classList.add("under-search");
 
-// map
+let statusLine = document.createElement("div");
+statusLine.id = "status-line";
+let dsOrderBy = document.createElement("p");
+dsOrderBy.innerHTML =
+  "Ordered By: <b>NAME</b>&#160;&#160;&#160;&#160;&#160;&#160;";
+dsOrderBy.id = "order-by";
+let span = document.createElement("span");
+
+let dsShowedRegion = document.createElement("p");
+dsShowedRegion.innerHTML = "Showed Region: <b>ALL</b>";
+dsShowedRegion.id = "showed-region";
+
+underSearch.appendChild(statusLine);
+statusLine.appendChild(dsOrderBy);
+statusLine.appendChild(span);
+span.appendChild(dsShowedRegion);
+document.body.appendChild(underSearch);
 
 // main content
 let mainContent = document.createElement("section");
@@ -129,6 +139,7 @@ document.body.appendChild(mainContent);
 let countryGrid = document.createElement("div");
 countryGrid.className = "country-grid";
 countryGrid.id = "country-grid";
+
 mainContent.appendChild(countryGrid);
 
 var map;
@@ -164,7 +175,7 @@ showMap = (latitude, longitude, area) => {
       layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
       view: new ol.View({
         center: ol.proj.fromLonLat([longitude, latitude]),
-        zoom: 11 - Math.log10(area / (16 - Math.log10(area))),
+        zoom: 10 - Math.log10(area / (16 - Math.log10(area))),
       }),
     });
 
@@ -189,29 +200,6 @@ closeMap = () => {
   x.remove();
   document.getElementById("map").style.visibility = "visible";
 };
-
-// searchCountries = () => {
-//   let input = document.querySelector(
-//     'div.search-sort-bar__input input[type="search"]'
-//   );
-
-//   let countryList = document.querySelectorAll(".country");
-//   //   input.onclick = () => (document.querySelector().innerHTML = )
-
-//   for (country of countryList) {
-//     if (
-//       country.children[1].children[0].innerHTML
-//         .toLowerCase()
-//         .substring(0, input.value.length) === input.value.toLowerCase()
-//     ) {
-//       country.classList.add("visible");
-//       country.classList.remove("invisible");
-//     } else {
-//       country.classList.add("invisible");
-//       country.classList.remove("visible");
-//     }
-//   }
-// };
 
 countryHTML = (
   flag,
@@ -238,32 +226,30 @@ countryHTML = (
   <div class="country-info">
   <h3 class="name">${name}</h3>
   <p>
-    Population:
+  <b>Population:</b>
     <span class="pop">${pop}</span>
   </p>
-  <p>Area: <span class="area">${area}</span> km&sup2;</p>
+  <p><b>Area:</b> <span class="area">${area}</span> km&sup2;</p>
   
-  <p>Region: <span class="region">${region}</span></p>
-  <p>Subregion: <span class="sRegion">${sRegion}</span></p>
+  <p><b>Region:</b> <span class="region">${region}</span></p>
+  <p><b>Subregion:</b> <span class="sRegion">${sRegion}</span></p>
   <p>
-    Lat: <span class="lat">${lat}</span> Lon:
-    <span class="lon">${lon} </span><span><button class="MapBtn" onclick="showMap(${lat}, ${lon}, ${area})">Map</button> </span>
+  <b>Lat:</b> <span class="lat">${lat}</span> <b>Lon:</b>
+    <span class="lon">${lon} </span><span><button class="MapBtn" 
+    onclick="showMap(${lat}, ${lon}, ${area})"><b>Map</b></button> </span>
   </p>
   
   <p>
-    Code: <span class="code">${code}</span> 
-    Calling Code: <span class="cCode">${cCode}</span>
+  <b>Code:</b> <span class="code">${code}</span> 
+  <b>Calling Code:</b> <span class="cCode">${cCode}</span>
   </p>
   </div>
   </div>
   `;
 };
 
-const inputSearch = document.getElementById("search-element");
 const countryList = document.getElementById("country-grid");
-
-let inputTerm = "";
-let countryData;
+var countryData;
 
 const fetchCountryData = async () => {
   countryData = await fetch(
@@ -271,55 +257,88 @@ const fetchCountryData = async () => {
   ).then((result) => result.json());
 };
 
-// inputSearch.addEventListener("keyup", (e) => {
-//   const inputTerm = e.target.value.toLowerCase();
-// });
+let inputTerm = "";
+let inputRegionTerm = "";
+let inputSortTerm = "";
 
-// searchValue = (element) => {
-//   if (event.keyCode == 13) {
-//     alert(element.value);
-//     countryData.filter((country) => {
-//       if (country.classList != undefined) {
-//         if (country.name.toLowerCase().includes(element.value.toLowerCase())) {
-//           console.log(country.classList);
-//           console.log(Array.isArray(countryData));
-//           country.classList.add("active");
-//           country.classList.remove("inactive");
-//         } else {
-//           console.log(country.classList);
-//           country.classList.remove("active");
-//           country.classList.add("inactive");
-//         }
-//       }
-//     });
-//   }
-// };
+compareArea = (a, b) => {
+  return b.area - a.area;
+};
+
+comparePopulation = (a, b) => {
+  return b.population - a.population;
+};
+
+let RESULTS = 0;
 
 const renderCountries = async () => {
   countryList.innerHTML = "";
-  //   console.log(countryList);
-  //   console.log(countryData);
-
   await fetchCountryData();
 
-  //   console.log(countryData);
+  // user input filter
+  if (inputTerm.length != 0) {
+    countryData = countryData.filter((country) => {
+      return country.name.toLowerCase().includes(inputTerm.toLowerCase());
+    });
+  }
 
-  countryData.filter((country) => {
-    if (country.classList != undefined) {
-      console.log("COUNTRYNAME", country.name.toLowerCase());
-      console.log("INPUTNAME--", inputTerm.toLowerCase());
-
-      if (country.name.toLowerCase().includes(inputTerm.toLowerCase())) {
-        country.classList.add("active");
-      } else {
-        country.classList.add("inactive");
-      }
+  if (inputSortTerm.length != 0) {
+    switch (inputSortTerm) {
+      case "Area":
+        countryData = countryData.sort(compareArea);
+        break;
+      case "Population":
+        countryData = countryData.sort(comparePopulation);
+        break;
+      case "Name":
+        break;
     }
-    console.log("X", inputTerm);
-    console.log("Y", country.classList);
-    console.log("Z", countryData);
-  });
+    console.log("POPULATION", countryData);
+  }
 
+  if (inputRegionTerm.length != 0) {
+    countryData = countryData.filter((country) => {
+      switch (inputRegionTerm) {
+        case "All":
+          return country.region
+            .toLowerCase()
+            .includes(inputRegionTerm.toLowerCase());
+        case "Africa":
+          return country.region
+            .toLowerCase()
+            .includes(inputRegionTerm.toLowerCase());
+
+        case "Americas":
+          return country.region
+            .toLowerCase()
+            .includes(inputRegionTerm.toLowerCase());
+
+        case "Asia":
+          return country.region
+            .toLowerCase()
+            .includes(inputRegionTerm.toLowerCase());
+
+        case "Europe":
+          return country.region
+            .toLowerCase()
+            .includes(inputRegionTerm.toLowerCase());
+
+        case "Oceania":
+          return country.region
+            .toLowerCase()
+            .includes(inputRegionTerm.toLowerCase());
+
+        case "Polar":
+          return country.region
+            .toLowerCase()
+            .includes(inputRegionTerm.toLowerCase());
+      }
+    });
+  }
+
+  RESULTS = countryData.length;
+
+  // formatting of data
   countryData.forEach((country) => {
     let position = {
       latitude: "",
@@ -354,16 +373,69 @@ const renderCountries = async () => {
       )
     );
   });
-  //   searchValue();
 };
-// };
 
 renderCountries();
 
-inputSearch.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    inputTerm = e.target.value;
-    console.log(inputTerm);
-    renderCountries();
+const inputSearch = document.getElementById("search-element");
+
+const inputSort = document.getElementById("sort");
+const orderbyStatus = document.getElementById("order-by");
+
+const inputRegion = document.getElementById("region");
+const orderbyRegion = document.getElementById("showed-region");
+
+// inputSearch.addEventListener("keypress", (e) => {
+//   if (e.key === "Enter") {
+//     inputTerm = e.target.value;
+//     // console.log("ABCD", inputTerm);
+
+//     renderCountries();
+//   }
+// });
+
+inputSort.addEventListener("change", (e) => {
+  inputSortTerm = e.target.options[e.target.selectedIndex].text;
+  orderbyStatus.innerHTML =
+    "Ordered By: <b>" +
+    inputSortTerm.toUpperCase() +
+    "</b>&#160;&#160;&#160;&#160;&#160;&#160;";
+
+  console.log("ABCD", inputSortTerm);
+  renderCountries();
+});
+
+inputRegion.addEventListener("change", (e) => {
+  inputRegionTerm = e.target.options[e.target.selectedIndex].text;
+
+  orderbyRegion.innerHTML =
+    "Showed Region: <b>" + inputRegionTerm.toUpperCase() + "</b>";
+  console.log("ABCD", inputRegionTerm);
+  renderCountries();
+});
+
+let counterLOL = 0;
+document.getElementById("search-element").addEventListener("search", (e) => {
+  if (counterLOL % 2 == 1) {
+    inputSort.style.visibility = "visible";
+    inputRegion.style.visibility = "visible";
+    orderbyRegion.style.visibility = "visible";
+    orderbyStatus.style.visibility = "visible";
+    searchAmount.style.visibility = "collapse";
+    counterLOL++;
+  } else {
+    inputSort.style.visibility = "collapse";
+    inputRegion.style.visibility = "collapse";
+    orderbyRegion.style.visibility = "collapse";
+    orderbyStatus.style.visibility = "collapse";
+
+    let searchAmount = document.createElement("p");
+    searchAmount.id = "search-amount";
+    searchAmount.innerHTML = "Search Results: <b>" + RESULTS + "</b> Matches";
+
+    statusLine.appendChild(searchAmount);
+    counterLOL++;
   }
+  inputTerm = e.target.value;
+  renderCountries();
 });
